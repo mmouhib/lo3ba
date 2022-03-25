1718e5776310367d09febb4ee5f40e808e99ebbb
mouhib <iwritebadcode@github.com>
2022-03-25 22:30:31 +0100

    gitignoreÃ

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import Home from './routes/Home';
 import { SearchContextProvider } from './context/SearchContext';
 import { Route, Routes } from 'react-router-dom';
+import GamesCatalog from './routes/GamesCatalog';
+import Game from './models/Game';
 
 export const App = () => (
    <div style={{ width: '100vw', height: '100vh' }}>
       <SearchContextProvider>
          <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={<GamesCatalog />} />
+            {/*<Route path="/" element={<Home />} />*/}
          </Routes>
       </SearchContextProvider>
    </div>
--- src/routes/GamesCatalog.tsx
+++ src/routes/GamesCatalog.tsx
@@ -1,2 +1,23 @@
-export default 'x';
-git status
+import React, { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+export default function GamesCatalog(): JSX.Element {
+   const [data, setData] = useState<any>(null);
+
+   useEffect(() => {
+      axios
+         .get(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`)
+         .then((response: AxiosResponse): void => {
+            setData(response.data.results);
+         });
+   }, []);
+
+   return (
+      <button
+         onClick={() => {
+            console.log(data);
+         }}>
+         click
+      </button>
+   );
+}
