70907e998b87bfbbef6b816187ae428411708092
mouhib <iwritebadcode@github.com>
2022-04-15 00:09:13 +0100

    countries listÃ

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,19 @@ import { Route, Routes } from 'react-router-dom';
 import GamesCatalog from './routes/GamesCatalog';
 import Game from './models/Game';
 import GamePage from './components/GamePage';
+import SignUp from './forms/SignUp';
 
 export default function App(): JSX.Element {
-   // @ts-ignore
    // const {gamesData,setGamesData} = useContext(SearchContextProvider);
 
    return (
       <div style={{ width: '100vw', height: '100vh' }}>
          <SearchContextProvider>
             <Routes>
-               <Route path="/" element={<GamePage />} />
+               <Route path="/" element={<Home />} />
+               <Route path="/signup" element={<SignUp />} />
+               {/*<Route path="/" element={<GamePage />} />*/}
                {/*<Route path="/" element={<GamesCatalog />} />*/}
-               {/*<Route path="/" element={<Home />} />*/}
             </Routes>
          </SearchContextProvider>
       </div>
