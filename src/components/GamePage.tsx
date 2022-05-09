844507be20781b7f1e2487283545c6ce97fbbeca
mouhib <iwritebadcode@github.com>
2022-05-09 20:23:01 +0100

    fixed game background

--- src/components/GamePage.tsx
+++ src/components/GamePage.tsx
@@ -4,33 +4,21 @@ import '../styles/game-page.css';
 
 export default function GamePage(): JSX.Element {
    const [game, setGame] = useState<any>({});
-   const [color, setColor] = useState<boolean>(true);
 
    useEffect(() => {
       axios.get(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_API_KEY}`).then((response: AxiosResponse): void => {
-         setGame(response.data.results[2]);
+         setGame(response.data.results[1]);
       });
    }, []);
 
    return (
-      // style={{ backgroundImage: `url('${game.background_image}')` }}
       <div className="game-page-component">
-         {/*<div className="content">*/}
-         {/*   <h1>{game.name}</h1>*/}
-         {/*   <p>{game.rating_top}</p>*/}
-         {/*</div>*/}
          <div className="image-container">
             <img src={game.background_image} alt={game.name} />
          </div>
+         <div className="content">
+            <h1>{game.name}</h1>
+         </div>
       </div>
    );
-
-   // return (
-   //    <pg
-   //       onClick={() => {
-   //          console.log(game);
-   //       }}>
-   //       games
-   //    </p>
-   // );
 }
